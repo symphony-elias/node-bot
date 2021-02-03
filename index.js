72da1278e5edbbdfecdf15702f52d65f89aae0e5
Elias Croze <elias.croze@symphony.com>
2021-02-03 19:02:41 +0100

    Example to pass config from object

--- index.js
+++ index.js
@@ -3,21 +3,25 @@ const fs = require('fs');
 
 Symphony.setDebugMode(true);
 
-const botHearsSomething = (event, messages) => {
-  messages.forEach((message, index) => {
-    let reply_message = 'Hello ' + message.user.firstName + ', hope you are doing well!!'
-    Symphony.sendMessage(message.stream.streamId, reply_message, null, Symphony.MESSAGEML_FORMAT)
-  })
-}
-
-Symphony.initBot(__dirname + '/config.json')
+Symphony.initBotFromObjects({
+  sessionAuthHost: "devx3.symphony.com",
+  sessionAuthPort: 443,
+  keyAuthHost: "devx3.symphony.com",
+  keyAuthPort: 443,
+  podHost: "devx3.symphony.com",
+  podPort: 443,
+  agentHost: "devx3.symphony.com",
+  agentPort: 443,
+  authType: "rsa",
+  botPrivateKeyPath: "rsa/",
+  botPrivateKeyName: "fbbot_privatekey.pkcs8",
+  botUsername: "postman-ecroze",
+  botEmailAddress: "fake@symphony.com",
+  authTokenRefreshPeriod: "30"
+})
   .then((symAuth) => {
-    Symphony.getDatafeedEventsService(botHearsSomething)
-    Symphony.sendMessageWithAttachment('wgRsEKnI3CHnTJB_6c7y5X___oihoBpVdA',
+    Symphony.sendMessage('wgRsEKnI3CHnTJB_6c7y5X___oihoBpVdA',
       'Hello',
       null,
-      'mytext.png',
-      'application/octet-stream',
-      fs.createReadStream('/Users/elias.croze/Downloads/lenna.png'),
       Symphony.MESSAGEML_FORMAT);
   });
